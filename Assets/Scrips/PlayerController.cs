8a344df44c0e121652e2febf06de6802da33fb84
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 22:38:56 +0900

    create SimpleFollow.cs

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	CharacterController controller;
+	Animator animator;
+
+	Vector3 moveDirection = Vector3.zero;
+
+	public float gravity;
+	public float speedZ;
+	public float speedJump;
+
 	void Start () {
-		
+		// 必要なコンポーネントを自動取得
+		controller = GetComponent<CharacterController>();
+		animator = GetComponent<Animator> ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		// 地上にいる場合のみ操作を行う
+		if (controller.isGrounded) {
+			// Inputを検知して前にすすめる
+			if (Input.GetAxis ("Vertical") > 0.0f)
+				moveDirection.z = Input.GetAxis ("Vertical") * speedZ;
+			else
+				moveDirection.z = 0;
+
+			// 方向転換
+			transform.Rotate(0, Input.GetAxis("Horizontal") * 3, 0);
+
+			// ジャンプ
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = speedJump;
+				animator.SetTrigger ("Jump");
+			}
+		}
+			// 重力分の力を毎フレーム追加
+			moveDirection.y -= gravity * Time.deltaTime;
+
+			// 移動実行
+			Vector3 globalDirection = transform.TransformDirection(moveDirection);
+			controller.Move (globalDirection * Time.deltaTime);
+
+			// 移動後接地してたらY方向の速度はリセットする
+			if(controller.isGrounded) moveDirection.y = 0;
+
+			// 速度が０以上なら走っているフラグをtrueにする
+			animator.SetBool("run", moveDirection.z > 0.0f);
 	}
 }
