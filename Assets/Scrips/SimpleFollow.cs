1557cd3e7cb7f1984bd0e8badc9c15e570b29f31
TakaoWing <tt.wing2838783@gmail.com>
2017-04-11 22:52:43 +0900

    can move Camera

--- Assets/Scrips/SimpleFollow.cs
+++ Assets/Scrips/SimpleFollow.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class SimpleFollow : MonoBehaviour {
 
+	Vector3 diff;
+
+	public GameObject target; // 追従ターゲット
+	public float followSpeed;
+
 	void Start () {
-		
+		diff = target.transform.position - transform.position; // 追従距離の計算
 	}
 
-	void Update () {
-		
+	void LateUpdate () { // 全てのUpdate関数が終わった後に実行される
+		// 線形補間関数によるスムージング
+		transform.position = Vector3.Lerp (transform.position, target.transform.position - diff, Time.deltaTime * followSpeed);
 	}
 }
